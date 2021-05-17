ac560f7351f618353850b3d345104e4e66e885cf
Stanislav Garanzha <garanzha.s.a@gmail.com>
2021-05-18 01:40:59 +0300

    Update serializers.py

--- capitals/serializers.py
+++ capitals/serializers.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class CapitalSerializer(serializers.Serializer):
     capital_city = serializers.CharField(max_length=200)
     capital_population = serializers.IntegerField()
-    author = serializers.CharField(source='author', max_length=200)
+    author = serializers.CharField(source='author.username', max_length=200)
