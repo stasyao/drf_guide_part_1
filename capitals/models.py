7904f82905a3d5c561a644a2aa1fc313baab9fcd
stas <garanzha.s.a@gmail.com>
2021-05-31 12:10:45 +0300

    add user model

--- capitals/models.py
+++ capitals/models.py
@@ -3,11 +3,14 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 
+User = get_user_model()
+
+
 class Capital(models.Model):
     country = models.CharField('country', max_length=150)
     capital_city = models.CharField('capital', max_length=150)
     capital_population = models.IntegerField('population')
-    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.capital_city
