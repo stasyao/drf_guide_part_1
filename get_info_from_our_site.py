919ba63a0122aa1ef261035afa45ca3b87af83da
Stanislav Garanzha <garanzha.s.a@gmail.com>
2021-06-09 15:32:01 +0300

    Update get_info_from_our_site.py

--- get_info_from_our_site.py
+++ get_info_from_our_site.py
@@ -3,10 +3,10 @@ import requests
 url = 'http://localhost:8000/api/capitals/'  # Полный адрес эндпоинта
 response = requests.get(url)  # Делаем GET-запрос
 # Поскольку данные пришли в формате json, переведем их в python
-response_in_python = response.json()
+response_on_python = response.json()
 # Запишем полученные данные в файл capitals.txt
 with open('capitals.txt', 'w') as file:
-    for capital in response_in_python:
+    for capital in response_on_python:
         file.write(
             f"The population of {capital['capital_city']} is "
             f"{capital['capital_population']}, "
